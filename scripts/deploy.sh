a8dbe1bdf95902d5cd7de8211435bcddd0fe9873
cedafiso0403 <cedafiso@gmail.com>
2024-07-18 00:50:24 -0700

    chore: Update deploy script to include email information and environment variables in go.yml

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -17,12 +17,6 @@ ps aux | awk '/\/tmp\/go-/' | sed -n 's/  */ /gp' | cut -d ' ' -f 2 | xargs kill
 cd ~
 git clone git@github.com:cedafiso0403/vokki_cloud.git
 
-# Set environment variables
-export SUPABASE_API_KEY="$1"
-export DB_URL="$2"
-export FROM_EMAIL="$3"
-export FROM_EMAIL_PASSWORD="$4"
-
 # Build and run the Go application
 cd vokki_cloud
 go build ./...
