eb6c251a3ac4305bf0dd2c05fd6654555f1ba8e4
cedafiso0403 <cedafiso@gmail.com>
2024-08-13 00:37:34 -0700

    chore: Add route for terms and conditions page
    feat: terms and conditions template
    
    feat: templates for emails

---
+++ internal/handlers/term_and_conditions.go
@@ -0,0 +1,9 @@
+package handlers
+
+import "net/http"
+
+func TermAndConditions(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "internal/views/terms_and_conditions.html")
+
+	w.Write([]byte("Hello, World!"))
+}
