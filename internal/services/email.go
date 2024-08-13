ae0c09b3cb80b3a285e3de2e09b7bdc542f80007
cedafiso0403 <cedafiso@gmail.com>
2024-08-12 23:49:35 -0700

    feat: Refactor code structure, update dependencies, and remove unused email package. Add reset password route

--- internal/services/email.go
+++ internal/services/email.go
@@ -17,9 +17,68 @@ func SendVerificationEmail(user models.User, token string) error {
 
 	auth := smtp.PlainAuth("", from, appPassword, smtpHost)
 	toList := []string{user.Email}
+
 	subject := "Subject: Email Verification\n"
-	body := fmt.Sprintf("Please verify your email by clicking the following link: https://3.145.165.81%s/?token=%s", vokki_constants.RouteVerifyEmail, token)
-	msg := []byte(subject + "\n" + body)
+
+	headers := "MIME-version: 1.0;\nContent-Type: text/html; charset=\"UTF-8\";\n" +
+		"Priority: Urgent\n" +
+		"X-Priority: 1\n" +
+		"Importance: High\n"
+
+	body := fmt.Sprintf(`
+	<!DOCTYPE html>
+	<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Email Verification</title>
+		<style>
+			body {
+				font-family: Arial, sans-serif;
+				background-color: #f4f4f4;
+				margin: 0;
+				padding: 0;
+			}
+			.container {
+				width: 100%%;
+				max-width: 600px;
+				margin: 0 auto;
+				padding: 20px;
+				background-color: #ffffff;
+				border-radius: 8px;
+				box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+			}
+			h1 {
+				color: #333333;
+			}
+			p {
+				color: #555555;
+			}
+			a {
+				display: inline-block;
+				padding: 10px 20px;
+				background-color: #007bff;
+				text-decoration: none;
+				border-radius: 5px;
+			}
+			span {
+				color: #ffffff;
+			}
+			a:hover {
+				background-color: #0056b3;
+			}
+		</style>
+	</head>
+	<body>
+		<div class="container">
+			<h1>Email Verification</h1>
+			<p>Please verify your email by clicking the following link:</p>
+			<p><a href="https://vokki.net%s/?token=%s"><span>Verify Email<span></a></p>
+		</div>
+	</body>
+	</html>
+`, vokki_constants.RouteVerifyEmail, token)
+
+	msg := []byte(subject + headers + "\n" + body)
 
 	err := smtp.SendMail(smtpHost+":"+smtpPort, auth, from, toList, msg)
 	if err != nil {
