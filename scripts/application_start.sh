8673e976fa9a47839d51468759d68cde9bff9b77
cedafiso0403 <cedafiso@gmail.com>
2024-08-16 14:26:58 -0700

    Update application_start.sh script to use absolute path for vokki_cloud directory

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -2,6 +2,14 @@ echo "Application Start Script..."
 
 cd /opt/vokki_cloud
 
+
+if ! command -v go &> /dev/null
+then
+    echo "No Go installed."
+else
+    echo "Go is already installed."
+fi
+
 nohup go run ./cmd/main.go
 
 
