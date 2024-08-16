b28eab7f4255eaf3e5d28d03b2738e0c7c56de1c
cedafiso0403 <cedafiso@gmail.com>
2024-08-16 14:58:12 -0700

    Update application_start.sh script to install Go if not already installed

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -3,12 +3,22 @@ echo "Application Start Script..."
 cd /opt/vokki_cloud
 
 
+# Check if Go is installed
 if ! command -v go &> /dev/null
 then
-    echo "No Go installed."
+    echo "Go is not installed. Installing Go..."
+    wget https://golang.org/dl/go1.21.0.linux-amd64.tar.gz
+    sudo tar -C /usr/local -xzf go1.21.0.linux-amd64.tar.gz
+    
+    # Add Go to PATH for the current session
+    export PATH=$PATH:/usr/local/go/bin
+    echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.bashrc
+    
+    echo "Go installed successfully."
 else
     echo "Go is already installed."
 fi
+
 
 nohup go run ./cmd/main.go
 
