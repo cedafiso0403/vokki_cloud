5bf2cc7e75dafd0137d18783a03172ef3cabc27f
cedafiso0403 <cedafiso@gmail.com>
2024-08-21 04:09:19 -0700

    chore: Set environment variables in application_start.sh script

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -18,11 +18,21 @@ else
     echo "Go is already installed."
 fi
 
-echo $DB_URL
+# Fetch secrets from AWS Secrets Manager
+SUPABASE_API_KEY_SCRIPT=$(aws secretsmanager get-secret-value --secret-id prod/vokki_cloud --query "SecretString" --output text | jq -r '.SUPABASE_API_KEY')
+DB_URL_SCRIPT=$(aws secretsmanager get-secret-value --secret-id prod/vokki_cloud --query "SecretString" --output text | jq -r '.DB_URL')
+FROM_EMAIL_SCRIPT=$(aws secretsmanager get-secret-value --secret-id prod/vokki_cloud --query "SecretString" --output text | jq -r '.FROM_EMAIL')
+FROM_EMAIL_PASSWORD_SCRIPT=$(aws secretsmanager get-secret-value --secret-id prod/vokki_cloud --query "SecretString" --output text | jq -r '.FROM_EMAIL_PASSWORD')
 
+# Set environment variables
+export SUPABASE_API_KEY="${SUPABASE_API_KEY_SCRIPT}"
+export DB_URL="${DB_URL_SCRIPT}"
+export FROM_EMAIL="${FROM_EMAIL_SCRIPT}"
+export FROM_EMAIL_PASSWORD="${FROM_EMAIL_PASSWORD_SCRIPT}"
+
+echo "Environment variables set"
 
 nohup go run ./cmd/main.go > /dev/null 2>&1 &
-
 
 echo "Application Start Script Completed"
 
--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -28,18 +28,4 @@ else
     echo "No existing Go processes found"
 fi
 
-# Fetch secrets from AWS Secrets Manager
-SUPABASE_API_KEY_SCRIPT=$(aws secretsmanager get-secret-value --secret-id prod/vokki_cloud --query "SecretString" --output text | jq -r '.SUPABASE_API_KEY')
-DB_URL_SCRIPT=$(aws secretsmanager get-secret-value --secret-id prod/vokki_cloud --query "SecretString" --output text | jq -r '.DB_URL')
-FROM_EMAIL_SCRIPT=$(aws secretsmanager get-secret-value --secret-id prod/vokki_cloud --query "SecretString" --output text | jq -r '.FROM_EMAIL')
-FROM_EMAIL_PASSWORD_SCRIPT=$(aws secretsmanager get-secret-value --secret-id prod/vokki_cloud --query "SecretString" --output text | jq -r '.FROM_EMAIL_PASSWORD')
-
-# Set environment variables
-export SUPABASE_API_KEY="${SUPABASE_API_KEY_SCRIPT}"
-export DB_URL="${DB_URL_SCRIPT}"
-export FROM_EMAIL="${FROM_EMAIL_SCRIPT}"
-export FROM_EMAIL_PASSWORD="${FROM_EMAIL_PASSWORD_SCRIPT}"
-
-echo "Environment variables set"
-
 echo "Before Install Script complete."
