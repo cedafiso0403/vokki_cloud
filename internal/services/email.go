f46bf26b20e2382d2493a440949958b2cec79470
Sofya Noskova <sofya.noskova@gmail.com>
2024-08-24 15:19:42 -0700

    fix: passed variables values to email templated from email.go chore: created reusable sendEmail function and common data structure for email templates chore: removed renaming vokki_constants package, since it's already it's name

--- internal/services/email.go
+++ internal/services/email.go
@@ -6,20 +6,39 @@ import (
 	"log"
 	"net/smtp"
 	"os"
-	vokki_constants "vokki_cloud/internal/constants"
+	"vokki_cloud/internal/constants"
 	"vokki_cloud/internal/models"
 )
 
-func SendVerificationEmail(user models.User, token string) error {
+// Common data structure for email templates
+type EmailData struct {
+	Route      string
+	Token      string
+	BaseScheme string
+	BaseHost   string
+	BasePath   string
+}
+
+// Helper function to populate shared data
+func getEmailData(route, token string) EmailData {
+	return EmailData{
+		Route:      route,
+		Token:      token,
+		BaseScheme: vokki_constants.BaseScheme,
+		BaseHost:   vokki_constants.BaseHost,
+		BasePath:   vokki_constants.BasePath,
+	}
+}
+
+// Common function to send emails
+func sendEmail(to string, subject string, tmplPath string, data EmailData) error {
 	from := os.Getenv("FROM_EMAIL")
 	appPassword := os.Getenv("FROM_EMAIL_PASSWORD")
 	smtpHost := "smtp.gmail.com"
 	smtpPort := "587"
 
 	auth := smtp.PlainAuth("", from, appPassword, smtpHost)
-	toList := []string{user.Email}
-
-	subject := "Subject: Email Verification\n"
+	toList := []string{to}
 
 	headers := "MIME-version: 1.0;\nContent-Type: text/html; charset=\"UTF-8\";\n" +
 		"Priority: Urgent\n" +
@@ -27,7 +46,7 @@ func SendVerificationEmail(user models.User, token string) error {
 		"Importance: High\n"
 
 	// Parse the HTML template file
-	tmpl, err := template.ParseFiles("internal/views/email_verification.html")
+	tmpl, err := template.ParseFiles(tmplPath)
 	if err != nil {
 		log.Println("Error parsing template: ", err)
 		return err
@@ -35,15 +54,6 @@ func SendVerificationEmail(user models.User, token string) error {
 
 	// Create a buffer to hold the executed template
 	var body bytes.Buffer
-
-	// Data to pass to the template
-	data := struct {
-		Route string
-		Token string
-	}{
-		Route: vokki_constants.RouteVerifyEmail,
-		Token: token,
-	}
 
 	// Execute the template and store it in the buffer
 	err = tmpl.Execute(&body, data)
@@ -53,7 +63,7 @@ func SendVerificationEmail(user models.User, token string) error {
 	}
 
 	// Combine subject, headers, and body into the final message
-	msg := []byte(subject + headers + "\n" + body.String())
+	msg := []byte("Subject: " + subject + "\n" + headers + "\n" + body.String())
 
 	// Send the email
 	err = smtp.SendMail(smtpHost+":"+smtpPort, auth, from, toList, msg)
@@ -65,57 +75,14 @@ func SendVerificationEmail(user models.User, token string) error {
 	return nil
 }
 
+// SendVerificationEmail sends a verification email to the user
+func SendVerificationEmail(user models.User, token string) error {
+	data := getEmailData(vokki_constants.RouteVerifyEmail, token)
+	return sendEmail(user.Email, "Email Verification", "internal/views/email_verification.html", data)
+}
+
+// SendPasswordResetEmail sends a password reset email to the user
 func SendPasswordResetEmail(user models.User, token string) error {
-	from := os.Getenv("FROM_EMAIL")
-	appPassword := os.Getenv("FROM_EMAIL_PASSWORD")
-	smtpHost := "smtp.gmail.com"
-	smtpPort := "587"
-
-	auth := smtp.PlainAuth("", from, appPassword, smtpHost)
-	toList := []string{user.Email}
-
-	subject := "Subject: Create New Password\n"
-
-	headers := "MIME-version: 1.0;\nContent-Type: text/html; charset=\"UTF-8\";\n" +
-		"Priority: Urgent\n" +
-		"X-Priority: 1\n" +
-		"Importance: High\n"
-
-	// Parse the HTML template file
-	tmpl, err := template.ParseFiles("internal/views/new_password.html")
-	if err != nil {
-		log.Println("Error parsing template: ", err)
-		return err
-	}
-
-	// Create a buffer to hold the executed template
-	var body bytes.Buffer
-
-	// Data to pass to the template
-	data := struct {
-		Route string
-		Token string
-	}{
-		Route: vokki_constants.RouteCreateNewPassword,
-		Token: token,
-	}
-
-	// Execute the template and store it in the buffer
-	err = tmpl.Execute(&body, data)
-	if err != nil {
-		log.Println("Error executing template: ", err)
-		return err
-	}
-
-	// Combine subject, headers, and body into the final message
-	msg := []byte(subject + headers + "\n" + body.String())
-
-	// Send the email
-	err = smtp.SendMail(smtpHost+":"+smtpPort, auth, from, toList, msg)
-	if err != nil {
-		log.Println("Error sending email: ", err)
-		return err
-	}
-
-	return nil
+	data := getEmailData(vokki_constants.RouteCreateNewPassword, token)
+	return sendEmail(user.Email, "Create New Password", "internal/views/new_password.html", data)
 }
