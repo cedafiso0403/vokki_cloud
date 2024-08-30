bd8cd7c5332a10d5369f2dc22c67bb7cfea8c738
cedafiso0403 <cedafiso@gmail.com>
2024-08-29 21:30:53 -0700

    chore: Update baseHost, basePath, and BaseScheme constants

--- internal/constants/constants.go
+++ internal/constants/constants.go
@@ -5,16 +5,11 @@ type ContextKey string
 type TokenType string
 
 // Base path for the API
-const (
-	BaseHost = "vokki.net"
-	BasePath = "/api/v1"
-	BaseScheme = "https"
-)
 
 const (
-	BaseHost   = "vokki.net"
-	BasePath   = "/api/v1"
-	BaseScheme = "https"
+	BaseHost   string = "vokki.net"
+	BasePath   string = "/api/v1"
+	BaseScheme string = "https"
 )
 
 const (
